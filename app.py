dc93983d784788469ed60fc583d2dec4945a6300
sonichigo <pathakanimesh12@gmail.com>
2021-05-25 16:33:19 +0530

    make it better

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash,json
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import os
+import uuid
 
 app = Flask(__name__)
 
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-
 
 app.config['MYSQL_HOST'] = 'api1219.mysql.database.azure.com'
 app.config['MYSQL_USER'] = 'Asuna1219@api1219'
@@ -15,6 +16,11 @@ app.config['MYSQL_PASSWORD'] = 'Asuna2001'
 app.config['MYSQL_DB'] = 'USER'
 
 mysql = MySQL(app)
+app.config['UPLOAD_FOLDER'] = 'static/Uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
 
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
@@ -30,7 +36,7 @@ def login():
 			session['loggedin'] = True
 			session['username'] = account['username']
 			msg = 'Logged in successfully !'
-			return render_template('index.html', msg = msg)
+			return render_template('userHome.html', msg = msg)
 		else:
 			msg = 'Incorrect username / password !'
 	return render_template('login.html', msg = msg)
@@ -66,6 +72,41 @@ def register():
 	elif request.method == 'POST':
 		msg = 'Please fill out the form !'
 	return render_template('register.html', msg = msg)
+	
+@app.route('/userHome')
+def userHome():
+        return render_template('userHome.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	if request.method=='POST':
+		file = request.files['file']
+		extension =os.path.splitext(file.filename)[1]
+		f_name = str(uuid.uuid4()) + extension
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'],f_name))
+		return json.dumps({'filename':f_name})
+
+
+
+@app.route('/addWish',methods=['GET','POST'])
+def add():
+	return render_template('addWish.html')
+def addWish():
+		if request.method == 'POST' and 'inputTitle' in request.form and 'inputDescription' in request.form:
+			_title = request.form['inputTitle']
+			_description = request.form['inputDescription']
+			_user = session.get('user')
+			datetime = os.datetime.now
+			cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+			cursor.execute('INSERT INTO tbl_post VALUES (%s, %s, %s, %s)', (_title, _description, _user, datetime))
+			mysql.connection.commit()
+			mysql.connection.close()
+			msg=''
+			msg='Published successfully!'
+			return redirect('/userHome',msg=msg)
+        
+		else:
+			return render_template('error.html',error = 'An error occurred!')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
